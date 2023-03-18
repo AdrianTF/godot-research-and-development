ebc2c3d586e1dcfbcc32f798078aa41b81ded420
AdrianTF <adrian_tf96@hotmail.com>
2023-03-18 18:58:36 +0100

    Player movement refactor and file directory improvement

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,21 +1,34 @@
 extends KinematicBody2D
 
-var gravity = 300
+var gravity = 1000
 var velocity = Vector2.ZERO
-var maxHorizontalSpeed = 100
-var jumpSpeed = 100
+var maxHorizontalSpeed = 140
+var horizontalAcceleration = 2000
+var jumpSpeed = 360
+var jumpTerminationMultiplier = 4
 
 func _ready():
 	pass 
 
 func _process(delta):
-	var moveVector = Vector2.ZERO
-	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
-	velocity.x = moveVector.x * maxHorizontalSpeed
+	var moveVector = get_movement_vector()
+	velocity.x +=moveVector.x * horizontalAcceleration * delta
+	if (moveVector.x == 0):
+		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
+	
+	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
 	
 	if(moveVector.y < 0 && is_on_floor()):
 		velocity.y = moveVector.y * jumpSpeed
 	
-	velocity.y += gravity * delta
+	if(velocity.y < 0 && !Input.is_action_pressed("jump")):
+		velocity.y += gravity * jumpTerminationMultiplier * delta
+	else:
+		velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+func get_movement_vector():
+	var moveVector = Vector2.ZERO
+	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	return moveVector
