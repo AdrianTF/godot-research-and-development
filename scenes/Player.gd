a93afc82cf394d52d07fee776b5bfb79cb61ca8d
AdrianTF <adrian_tf96@hotmail.com>
2023-03-19 10:37:50 +0100

    idle, jump and run animated sprites added

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -26,9 +26,28 @@ func _process(delta):
 	else:
 		velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	update_animation()
 
 func get_movement_vector():
 	var moveVector = Vector2.ZERO
 	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	return moveVector
+
+func update_animation():
+	var movementVector = get_movement_vector()
+	
+	if (!is_on_floor()):
+		$AnimatedSprite.play("jump")
+	elif (movementVector.x != 0):
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+	
+	if (movementVector.x != 0):
+		$AnimatedSprite.flip_h = true if movementVector.x < 0 else false
+
+
+
+
