bf465a6d37ddd46ebbe0712d910b4d0085a36504
AdrianTF <adrian_tf96@hotmail.com>
2023-03-19 11:45:53 +0100

    Coin pickup animation added

--- scenes/collectables/Coin.gd
+++ scenes/collectables/Coin.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 func _ready():
 	$Area2D.connect("area_entered", self, "on_area_entered")
+	$AnimatedSprite.play("default")
 
 func on_area_entered(_area2d):
-	queue_free()
+	$AnimationPlayer.play("pickup")
+	call_deferred("disable_pickup")
+
+func disable_pickup():
+	$Area2D/CollisionShape2D.disabled = true
