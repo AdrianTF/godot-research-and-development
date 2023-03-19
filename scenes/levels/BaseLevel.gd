921cf44792f311d5a17f2bd038e56be7664a0526
AdrianTF <adrian_tf96@hotmail.com>
2023-03-19 13:03:37 +0100

    Player death and respawn added

---
+++ scenes/levels/BaseLevel.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var playerScene = preload("res://scenes/player/Player.tscn")
+var spawnPosition = Vector2.ZERO
+var currentPlayerNode = null
+
+func _ready():
+	spawnPosition = $Player.global_position
+	register_player($Player)
+
+func register_player(player):
+	currentPlayerNode = player
+	currentPlayerNode.connect("died", self, "on_player_died", [], CONNECT_DEFERRED)
+
+func create_player():
+	var playerInstance = playerScene.instance()
+	add_child_below_node(currentPlayerNode, playerInstance)
+	playerInstance.global_position = spawnPosition
+	register_player(playerInstance)
+
+func on_player_died():
+	currentPlayerNode.queue_free()
+	create_player()
