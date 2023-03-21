4b5e6350e328bf635508c311b4c8bd7e4cf43057
AdrianTF <adrian_tf96@hotmail.com>
2023-03-21 19:44:43 +0100

    Attack now kills enemies, base level structure added

--- scenes/enemies/Enemy.gd
+++ scenes/enemies/Enemy.gd
@@ -11,7 +11,7 @@ var gravity = 500
 func _ready():
 	direction = Vector2.RIGHT if startDirection == Direction.RIGHT else Vector2.LEFT
 	$GoalDetector.connect("area_entered", self, "on_goal_entered")
-
+	$HitboxArea.connect("area_entered", self, "on_hitbox_entered")
 func _process(delta):
 	velocity.x = (direction * maxSpeed).x
 	
@@ -22,3 +22,6 @@ func _process(delta):
 
 func on_goal_entered(_area2d):
 	direction *= -1
+
+func on_hitbox_entered(_area2d):
+	queue_free()
