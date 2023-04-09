90045c511a06c1eb301c0e856f293bc36ec32213
AdrianTF <adrian_tf96@hotmail.com>
2023-04-09 10:53:33 +0200

    Pause menu added.

---
+++ scenes/UI/PauseMenu.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+onready var continueButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/ContinueButton
+onready var optionsButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/OptionsButton
+onready var quitButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/QuitButton
+
+func _ready():
+	continueButton.connect("pressed", self, "on_continue_pressed")
+	optionsButton.connect("pressed", self, "on_options_pressed")
+	quitButton.connect("pressed", self, "on_quit_pressed")
+	get_tree().paused = true
+
+func _unhandled_input(event):
+	if(event.is_action_pressed("pause")):
+		unpause()
+		get_tree().set_input_as_handled()
+
+func unpause():
+	queue_free()
+	get_tree().paused = false
+
+func on_continue_pressed():
+	unpause()
+
+func on_options_pressed():
+	pass
+
+func on_quit_pressed():
+	$"/root/ScreenTransitionManager".transition_to_scene("res://scenes/UI/MainMenu.tscn")
+	unpause()
--- scenes/levels/BaseLevel.gd
+++ scenes/levels/BaseLevel.gd
@@ -5,6 +5,7 @@ signal coin_total_changed
 export(PackedScene) var levelCompleteScene
 
 var playerScene = preload("res://scenes/player/Player.tscn")
+var pauseScene = preload("res://scenes/UI/PauseMenu.tscn")
 var spawnPosition = Vector2.ZERO
 var currentPlayerNode = null
 var totalCoins = 0
@@ -16,6 +17,11 @@ func _ready():
 	coin_total_changed(get_tree().get_nodes_in_group("coin").size())
 	
 	$Door.connect("player_won", self, "on_player_won")
+
+func _unhandled_input(event):
+	if(event.is_action_pressed("pause")):
+		var pauseInstance = pauseScene.instance()
+		add_child(pauseInstance)
 
 func coin_collected():
 	collectedCoins += 1
