8462bba1837a52cb710ad7e82ae4242f53a55158
AdrianTF <adrian_tf96@hotmail.com>
2023-04-10 15:45:00 +0200

    Coin pickup SFX

--- scenes/collectables/Coin.gd
+++ scenes/collectables/Coin.gd
@@ -6,6 +6,7 @@ func _ready():
 
 func on_area_entered(_area2d):
 	$AnimationPlayer.play("pickup")
+	$RandomAudioStreamPlayer.play()
 	call_deferred("disable_pickup")
 	
 	var baseLevel = get_tree().get_nodes_in_group("base_level")[0]
